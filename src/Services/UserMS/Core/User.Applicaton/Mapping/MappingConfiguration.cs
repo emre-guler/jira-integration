e462af32ba211af2bf2dface25add19a9c2ff1ec
emreguler <emre_guler01@hotmail.com>
2023-02-09 03:35:49 +0300

    Mapping configurations added.

---
+++ src/Services/UserMS/Core/User.Applicaton/Mapping/MappingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using FastExpressionCompiler;
+using Mapster;
+
+namespace User.Applicaton.Mapping;
+
+public static class MappingConfiguration
+{
+    public static TypeAdapterConfig Generate()
+    {
+        var config = new TypeAdapterConfig();
+
+        // Configurations here...
+
+        config.Compiler = exp => exp.CompileFast();
+        config.Compile();
+
+        return config;
+    }
+}
--- src/Services/UserMS/Core/User.Applicaton/ServiceRegistration.cs
+++ src/Services/UserMS/Core/User.Applicaton/ServiceRegistration.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using User.Applicaton.Mapping;
 using User.Applicaton.Middlewares;
 
 namespace User.Applicaton;
 
 public static class ServiceRegistration
 {
-    public static void AddApplicationRegistration(this WebApplication app)
+    public static void AddApplicationRegistration(this WebApplication app, IServiceCollection services)
     {
+        // Exception Middleware
         app.UseMiddleware<ExceptionMiddleware>();
+
+        // Mapster 
+        TypeAdapterConfig mapConfig = MappingConfiguration.Generate();
+        services.AddSingleton(mapConfig);
+        services.AddSingleton<IMapper, ServiceMapper>();
     }
 }
